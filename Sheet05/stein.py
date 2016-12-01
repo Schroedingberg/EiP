e4446d60dd8687e520ecdce92eb925e83c50e87f
Schroedingberg <aaronrebmann@gmail.com>
2016-12-01 20:39:22 +0100

    Finished stein implementation with input all at once acception

--- Sheet05/stein.py
+++ Sheet05/stein.py
@@ -13,31 +13,26 @@ def stein_rec(A, B, k=0):
     A or B being zero. In this case, the larger one of both i.e. the
     one that is not zero, multiplied with 2^k is returned, according
     to the given description of the algorithm.  k is provided as an
-    argument.
+    argument, however
     """
-
     if A == 0:
-        print("A==0")
         return B*2**k
     if B == 0:
-        print("B==0")
         return A*2**k
     if even(A) and even(B):
-        print("A and B even")
         return stein_rec(A/2, B/2, k+1)
     if even(A) and not even(B):
-        print("A even, B not")
         return stein_rec(A/2, B, k)
     if even(B) and not even(A):
-        print("B even not A")
         return stein_rec(A, B/2, k)
     elif A >= B:
-        print("A >= B")
         return stein_rec((A-B)/2, B, k)
     elif B > A:
-        print("B> A")
         return stein_rec(A, (B-A)/2, k)
 
-if __name__ == "__main__":
-    valuelist = [3528, 3780]
-    print(stein_rec(valuelist[0], valuelist[1]))
+values = input()
+valuelist = values.split("\n")
+n = int(valuelist[0])+1
+ABs = [[int(i) for i in AB.split(" ")]for AB in valuelist[1:]]
+for zahlenpaar in ABs:
+    print(stein_rec(zahlenpaar[0], zahlenpaar[1], 0))
